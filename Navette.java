d0761ba74edf51e5c612844c26f02beea953af81
JeromeSoV <jeromesauve28.js@gmail.com>
2024-12-02 11:32:48 +0100

    Add files via upload

---
+++ Navette.java
@@ -0,0 +1,13 @@
+class Navette{
+    String nom;
+    int nivOxy;
+    int nivElec;
+    int nivTemp;
+    int nivTempMAX;
+    int nivElecMAX;
+    int nivOxyMAX;
+    int perteElec;
+    int perteOxy;
+    int perteTemp;
+    int fatigue;
+}
