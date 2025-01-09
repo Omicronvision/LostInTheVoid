4c60c580d95b8845156202690f133168a0176cf6
JeromeSoV <jeromesauve28.js@gmail.com>
2025-01-09 10:39:31 +0100

    Delete BonusMalus.java

--- BonusMalus.java
+++
@@ -1,5 +0,0 @@
-class BonusMalus{
-    String[] casPossibles;
-    int[][] impactMax; //tableau de taille int[3][length(casPossibles)], les lignes selon la ressource concernée 
-    int[][] perteMax;  //tableau de taille int[3][length(casPossibles)], les lignes selon la ressource concernée 
-}
