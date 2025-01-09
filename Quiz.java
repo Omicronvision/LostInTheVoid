ffd4a068233bc367502903e9dea0baf3af10fafc
JeromeSoV <jeromesauve28.js@gmail.com>
2025-01-09 10:37:14 +0100

    Delete Quiz.java

--- Quiz.java
+++
@@ -1,4 +0,0 @@
-class Quiz{
-    String[][] quiz;
-    int questionPrecedente; //pourquoi pas stocker l'indice de la question en fin de question, pour éviter toute répétion de la même question 2 fois de suite.
-}
